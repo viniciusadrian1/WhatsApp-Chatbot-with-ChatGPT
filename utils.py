d8f626cef1c8b898984e3953d906dab177e0a7db
viniciusadrian1 <vinicius97adrian@outlook.com>
2025-07-10 23:37:28 -0300

    beyondbox-bot

--- utils.py
+++ utils.py
@@ -1,18 +1,18 @@
-import openai
+from openai import OpenAI
 import os
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 def responder_chatgpt(mensagem):
     prompt = f"""
 Você é o assistente da BeyondBox, uma empresa que vende caixas cartonadas e produtos personalizados como canecas, camisetas e copos. Responda com simpatia e objetividade:
 '{mensagem}'
 """
-    resposta = openai.ChatCompletion.create(
+    resposta = client.chat.completions.create(
         model="gpt-4o",
         messages=[
             {"role": "system", "content": "Você é um atendente da BeyondBox."},
             {"role": "user", "content": prompt}
         ]
     )
-    return resposta['choices'][0]['message']['content'].strip()
+    return resposta.choices[0].message.content.strip()
